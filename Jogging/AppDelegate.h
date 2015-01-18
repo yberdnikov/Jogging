413ab67a6b8ddaf0242e4062b4b357cbfec29ed6
Yuriy B <yberdnikov@me.com>
2015-01-18 12:31:24 +0200

    Initial Commit

---
+++ Jogging/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jogging
+//
+//  Created by Yuriy Berdnikov on 1/18/15.
+//  Copyright (c) 2015 Yuriy Berdnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
