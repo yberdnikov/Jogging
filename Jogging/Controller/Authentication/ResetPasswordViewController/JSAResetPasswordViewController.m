cfb3b110aad78bd9cfb0360cfc10fd5c1be3f65b
Yuriy B <yberdnikov@me.com>
2015-01-18 15:13:36 +0200

    Added Reset password functionality.

--- Jogging/Controller/Authentication/ResetPasswordViewController/JSAResetPasswordViewController.m
+++ Jogging/Controller/Authentication/ResetPasswordViewController/JSAResetPasswordViewController.m
@@ -1,47 +1,37 @@
 //
-//  JSALoginViewController.m
+//  JSAResetPasswordViewController.m
 //  Jogging
 //
 //  Created by Yuriy Berdnikov on 1/18/15.
 //  Copyright (c) 2015 Yuriy Berdnikov. All rights reserved.
 //
 
-#import "JSALoginViewController.h"
+#import "JSAResetPasswordViewController.h"
 #import "NSString+Utilities.h"
 #import "UIAlertView+Blocks.h"
 #import <SVProgressHUD/SVProgressHUD.h>
 #import <Parse/Parse.h>
 
-@interface JSALoginViewController ()
+@interface JSAResetPasswordViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
-@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
 @end
 
-@implementation JSALoginViewController
+@implementation JSAResetPasswordViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    self.title = NSLocalizedString(@"Sign In", nil);
-    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Back", nil) style:UIBarButtonItemStylePlain target:nil action:nil];
+    
+    self.title = NSLocalizedString(@"Reset Password", nil);
     
     [self decorateUIElements];
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    
-    [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - UI Setup
@@ -49,26 +39,21 @@
 - (void)decorateUIElements
 {
     self.emailTextField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 15, CGRectGetHeight(self.emailTextField.bounds))];
-    self.passwordTextField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 15, CGRectGetHeight(self.emailTextField.bounds))];
     self.emailTextField.leftViewMode = UITextFieldViewModeAlways;
-    self.passwordTextField.leftViewMode = UITextFieldViewModeAlways;
 }
 
 #pragma mark - UITextFieldDelegate methods
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    if (textField == self.emailTextField)
-        [self.passwordTextField becomeFirstResponder];
-    else
-        [self.view endEditing:YES];
+    [self.view endEditing:YES];
     
     return YES;
 }
 
 #pragma mark - UIButton selectors
 
-- (IBAction)signinButtonPressed:(UIButton *)sender
+- (IBAction)resetPasswordButtonPressed:(UIButton *)sender
 {
     if (![self.emailTextField.text isValidEmailFormat])
     {
@@ -76,26 +61,20 @@
         return;
     }
     
-    if (![self.passwordTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]].length)
-    {
-        [UIAlertView showErrorWithMessage:NSLocalizedString(@"Please enter valid password", nil) handler:nil];
-        return;
-    }
-    
     [self.view endEditing:YES];
     
-    [self loginUser];
+    [self resetUserPassword];
 }
 
 #pragma mark - Server API communication
 
-- (void)loginUser
+- (void)resetUserPassword
 {
     [SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeGradient];
     
     __weak __typeof__(self) weakSelf = self;
-    [PFUser logInWithUsernameInBackground:self.emailTextField.text password:self.passwordTextField.text block:^(PFUser *user, NSError *error) {
-
+    [PFUser requestPasswordResetForEmailInBackground:self.emailTextField.text block:^(BOOL succeeded, NSError *error) {
+        
         [SVProgressHUD dismiss];
         
         if (error)
@@ -105,7 +84,9 @@
             return;
         }
         
-        [weakSelf.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+        [UIAlertView showWithTitle:NSLocalizedString(@"Email Sent", nil) message:NSLocalizedString(@"Follow the instructions in the email to access your password reset page", nil) handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
+            [weakSelf.navigationController popViewControllerAnimated:YES];
+        }];
     }];
 }
 
